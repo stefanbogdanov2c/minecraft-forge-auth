3ff8e0b07c1511a5eea93fb61ec6fb814efbe1c1
stefanbogdanov2c <sbogdanovic@synapticon.com>
2024-12-01 13:13:53 +0100

    Remove the auth from the command

--- src/main/java/com/example/authmod/AuthCommand.java
+++ src/main/java/com/example/authmod/AuthCommand.java
@@ -7,48 +7,51 @@ import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 
 public class AuthCommand {
-    public static LiteralArgumentBuilder<CommandSourceStack> registerCommands() {
-        return Commands.literal("auth")
-            .then(Commands.literal("register")
-                .then(Commands.argument("password", StringArgumentType.string())
-                    .executes(context -> {
-                        String username = context.getSource().getTextName();
-                        String password = StringArgumentType.getString(context, "password");
-                        if (PlayerAuthHandler.register(username, password)) {
-                            context.getSource().sendSuccess(() -> Component.literal("Registered successfully!"), false);
-                        } else {
-                            context.getSource().sendFailure(Component.literal("You are already registered!"));
-                        }
-                        return 1;
-                    })
-                )
-            )
-            .then(Commands.literal("login")
-                .then(Commands.argument("password", StringArgumentType.string())
-                    .executes(context -> {
-                        String username = context.getSource().getTextName();
-                        String password = StringArgumentType.getString(context, "password");
-                        if (PlayerAuthHandler.login(username, password)) {
-                            context.getSource().sendSuccess(() -> Component.literal("Logged in successfully!"), false);
-                        } else {
-                            context.getSource().sendFailure(Component.literal("Invalid username or password!"));
-                        }
-                        return 1;
-                    })
-                )
-            ).then(Commands.literal("changePassword")
-                .then(Commands.argument("password", StringArgumentType.string())
-                    .executes(context -> {
-                        String username = context.getSource().getTextName();
-                        String password = StringArgumentType.getString(context, "password");
-                        if (PlayerAuthHandler.changePassword(username, password)) {
-                            context.getSource().sendSuccess(() -> Component.literal("Password changed successfully!"), false);
-                        } else {
-                            context.getSource().sendFailure(Component.literal("Could not change password!"));
-                        }
-                        return 1;
-                    })
-                )
+    public static LiteralArgumentBuilder<CommandSourceStack> registerCommand() {
+        return Commands.literal("register")
+            .then(Commands.argument("password", StringArgumentType.string())
+                .executes(context -> {
+                    String username = context.getSource().getTextName();
+                    String password = StringArgumentType.getString(context, "password");
+                    if (PlayerAuthHandler.register(username, password)) {
+                        context.getSource().sendSuccess(() -> Component.literal("Registered successfully!"), false);
+                    } else {
+                        context.getSource().sendFailure(Component.literal("You are already registered!"));
+                    }
+                    return 1;
+                })
+            );
+    }
+
+    public static LiteralArgumentBuilder<CommandSourceStack> loginCommand() {
+        return Commands.literal("login")
+            .then(Commands.argument("password", StringArgumentType.string())
+                .executes(context -> {
+                    String username = context.getSource().getTextName();
+                    String password = StringArgumentType.getString(context, "password");
+                    if (PlayerAuthHandler.login(username, password)) {
+                        context.getSource().sendSuccess(() -> Component.literal("Logged in successfully!"), false);
+                    } else {
+                        context.getSource().sendFailure(Component.literal("Invalid username or password!"));
+                    }
+                    return 1;
+                })
+            );
+    }
+
+    public static LiteralArgumentBuilder<CommandSourceStack> changePasswordCommand() {
+        return Commands.literal("changePassword")
+            .then(Commands.argument("password", StringArgumentType.string())
+                .executes(context -> {
+                    String username = context.getSource().getTextName();
+                    String password = StringArgumentType.getString(context, "password");
+                    if (PlayerAuthHandler.changePassword(username, password)) {
+                        context.getSource().sendSuccess(() -> Component.literal("Password changed successfully!"), false);
+                    } else {
+                        context.getSource().sendFailure(Component.literal("Could not change password!"));
+                    }
+                    return 1;
+                })
             );
     }
 }
